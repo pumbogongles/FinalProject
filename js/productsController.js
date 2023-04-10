445c9f330fea69317f0ff273ee256a71b4310fe0
pumbogongles <roys.exe+github@gmail.com>
2023-04-10 11:28:03 +0800

    updated the following: all html with .js script, hardcoded items in productsController.js

---
+++ js/productsController.js
@@ -0,0 +1,51 @@
+//Hardcoded items
+
+const tote1 = {
+    type: "Tote", // 4 options: tote, backpack, briefcase or camera bag
+    name: "Basic Tote Bag", //any name
+    desc: "A simple tote bag for any occassion", // any description
+    color: "Red", // 6 options Red, Blue, Yellow, Green, Black, Brown
+    price: 19.90, // any price
+    imageURL: "images\Dalle2\singleProduct\_6cf57709-06ac-43db-87ca-bb6d7f70e3c1.jpg"  
+}
+
+const tote2 = {
+    type: "Tote", // 4 options: tote, backpack, briefcase or camera bag
+    name: "Basic Plus Tote Bag", //any name
+    desc: "A more complex tote bag for any occassion", // any description
+    color: "White", // 6 options Red, Blue, Yellow, Green, Black, Brown
+    price: 21.90, // any price
+    imageURL: "images\Dalle2\singleProduct\_8452f73d-8efc-47ea-894a-26e0ac7b8175.jpg"  
+}
+
+const backpack1 = {
+    type: "Backpack", // 4 options: tote, backpack, briefcase or camera bag
+    name: "Basic Backpack", //any name
+    desc: "A versatile backpack for any occassion", // any description
+    color: "Brown", // 6 options Red, Blue, Yellow, Green, Black, Brown
+    price: 39.90, // any price
+    imageURL: "images\Dalle2\singleProduct\_a45e3865-2a51-4482-8755-b0d7801efce2.jpg"  
+}
+
+const briefcase1 = {
+    type: "Briefcase", // 4 options: tote, backpack, briefcase or camera bag
+    name: "Working Briefcase", //any name
+    desc: "A work briefcase", // any description
+    color: "Black", // 6 options Red, Blue, Yellow, Green, Black, Brown
+    price: 50.90, // any price
+    imageURL: "images\Dalle2\singleProduct\_8452f73d-8efc-47ea-894a-26e0ac7b8175.jpg"  
+}
+
+const cameraBag1 = {
+    type: "Camera Bag", // 4 options: tote, backpack, briefcase or camera bag
+    name: "Outdoor Camera Bag", //any name
+    desc: "An average looking Camera Bag", // any description
+    color: "Blue", // 6 options Red, Blue, Yellow, Green, Black, Brown
+    price: 39.90, // any price
+    imageURL: "images\Dalle2\singleProduct\_ee00f514-3f45-4a84-8aee-61be8f012397.jpg"  
+}
+
+const productArray = [];
+
+productArray.push(tote1, backpack1, briefcase1, cameraBag1)
+
